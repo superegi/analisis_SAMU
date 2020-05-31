09478d4dc78808cfc05cd22cad2ca8d1b9ea9c64
Egidio <hight.esniif@gmail.com>
2020-05-30 21:22:54 -0400

    Gráficos actualizados con resultados

--- Analisis llamadas 131/Scripts/Funciones_descriptoras.py
+++ Analisis llamadas 131/Scripts/Funciones_descriptoras.py
@@ -1,110 +1,165 @@
-# Inicializo el lector standard
-exec(open('inicializador_standard.py').read())
-
-
-
 
 # Defino las funciones
 
-def descripcion_llamadas_general(inicio, fin, ax= None, **kwargs):
-    # inicio y fin son fechas 
-    db1 = BD.loc[
-        (BD.Fecha > pd.to_datetime(inicio)) &
-        (BD.Fecha < pd.to_datetime(fin))]
-    # Periodo de estudio ########################################################
-    dum_t1 = db1.Fecha.describe()['first']
-    dum_t2 = db1.Fecha.describe()['last']
 
-    # Cantidad de días del estudio ########################################################
-    dum_dt = db1.Fecha.describe()['last'] - db1.Fecha.describe()['first']
+# Función que corta dataset en fechas. Si no
+# se define fecha le suma 1 mes
+def corte_dataset(dataset, inicio= None, fin=None):
+    inicio = pd.to_datetime(inicio)
+    fin = pd.to_datetime(fin)
+    if inicio != None:
+        if fin !=None:
+            print('Seteado inicio y fin')
+            db1 = dataset.loc[
+                (dataset.Fecha > inicio) &
+                (dataset.Fecha < fin)]
+        else:
+            fin = inicio + timedelta(weeks=1)
+            print('Seteado inicio, NO fin')
+            db1 = dataset.loc[
+                (dataset.Fecha > inicio) &
+                (dataset.Fecha < fin)]
+    else:
+        print('No hay rango de fecha seteado. Se procede con todo')
+        db1 = dataset
+    
+    print('Rango de fecha: Inicio({}) y fin({})'.format(inicio, fin))
+    return db1
 
-    # Cantidad de llamadas en el estudio ############################################
-    dum_n = db1.Fecha.describe()['count']
-    print('El periodo de estudio fue desde el {} al {}. '
-        .format(dum_t1.strftime('%d-%b-%Y'), dum_t2.strftime('%d-%b-%Y'))
-        )
-    print('El tiempo de estudio corresponde a {} días, lo mismo que {} meses. '
-        .format(
-            dum_dt.days,
-            str(round(dum_dt.days/30,3))
-            )
-        )
-    print('')
-    print('La cantidad de llamadas en el periodo de estudio fue de {}llamadas,\
-    equivalente a {} llamadas diarias y {} mensuales.'
-        .format(
-            dum_n,
-            round(dum_n/pd.to_numeric(dum_dt.days)),
-            round(30*dum_n/pd.to_numeric(dum_dt.days))
-        )
-         )
-    print('')
+# Función descriptora de llamadas, normalizada por mes y día
+def tablaCDR_general(dataset, inicio= None, fin=None):
+    # Selecciono un rango de fechas para la función
+    db1 = corte_dataset(dataset, inicio, fin)
+    
+    # Hago las métricas
+    descripcion_dum = db1.Fecha.describe()
+    var = pd.Series([])
+    var['Primera observacion']  = descripcion_dum[4]
+    var['Ultima observacion']   = descripcion_dum[5]
+    var['Duracion en dias']     = (descripcion_dum[5] - descripcion_dum[4]).days
+    var['Duracion en meses']    = (descripcion_dum[5] - descripcion_dum[4]).days/30
+    var['Llamadas (n)']         = descripcion_dum[0]
+    var['Llamadas (n/mes)']     = var['Llamadas (n)']/var['Duracion en meses']
+    var['Llamadas (n/dia)']     = var['Llamadas (n)']/var['Duracion en dias']
+    
+    var['Llamadas Hacia SAMU/mes']         = db1.loc[db1.Tipo_llamada=='Hacia SAMU'].Fecha.count()/var['Duracion en meses']
+    var['Llamadas Hacia SAMU/dia']         = db1.loc[db1.Tipo_llamada=='Hacia SAMU'].Fecha.count()/var['Duracion en dias']
+    
+    var['Duración llamada prom (min)']     = db1.loc[db1.Tipo_llamada=='Hacia SAMU'].Duracion.mean()/60
+    
+    var['Llamadas traspasadas/mes']            = db1.loc[db1.Trapaso=='Si'].Fecha.count()/var['Duracion en meses']
+    var['Llamadas traspasadas/dia']        = db1.loc[db1.Trapaso=='Si'].Fecha.count()/var['Duracion en dias']
+    
+    var['Uso horario (%prom)']             = np.mean(          # corresponde al uso de 1 hora de CR
+        ((db1.groupby(db1.Fecha.dt.hour)).Duracion.sum())/(var['Duracion en dias']*60)/60 *100)
 
-def DLG_cantidad(inicio, fin, ax= None, **kwargs):
-    # inicio y fin son fechas 
-    db1 = BD.loc[
-        (BD.Fecha > pd.to_datetime(inicio)) &
-        (BD.Fecha < pd.to_datetime(fin))]
-    # Periodo de estudio ########################################################
-    dum_t1 = db1.Fecha.describe()['first']
-    dum_t2 = db1.Fecha.describe()['last']
+    var['Llamadas perdidas/mes']       = db1.loc[db1.Estado=='Perdida'].Fecha.count()/var['Duracion en meses']
+    var['Llamadas perdidas/dia']       = db1.loc[db1.Estado=='Perdida'].Fecha.count()/var['Duracion en dias']
 
-    # Cantidad de días del estudio ########################################################
-    dum_dt = db1.Fecha.describe()['last'] - db1.Fecha.describe()['first']
+    return var
+
+# Tabla indicadores
+def tabla_indicadores(dataset, start=None, **kwargs):
+    datelist1 = pd.date_range(start,**kwargs)
+    dictado = dict()
+    for x in datelist1:
+        dictado[
+            str(x.strftime("%d%b%y"))] = tablaCDR_general(dataset, x, fin=(pd.to_datetime(x)+datelist1.freq))
+    tab = pd.DataFrame(dictado).T
+    return tab
+
+#graficos desde la tabla
+def grafo_usoyllamadas(tabla, titulo=None):
+    fig,ax = plt.subplots()
+    ax.plot(tabla[['Uso horario (%prom)']], color="black", marker="o")
+    ax.set_ylim([50,600])
+    ax.set_xlabel("Periodo",fontsize=14)
+    ax.title.set_text(titulo)
+    plt.xticks(rotation='vertical')
+    ax.set_ylabel("Uso del CR en %",color="black",fontsize=14)
+
+    ax2=ax.twinx()
+    ax2.plot(tabla[['Llamadas (n/mes)']], color="#2ca02c", marker="x")
+    ax2.set_ylim([27000,39000])
+    ax2.set_ylabel("Cantidad llamadas (n/mes)",color="#2ca02c",fontsize=14)
+
+
+def grafo_perdidasyduracion(tabla, titulo=None):
+    fig,ax = plt.subplots()
+    tabla['Llamadas perdidas/mes'].plot(kind='bar', color='#1f77b4')
+    ax.set_ylim([100,1200])
+    ax.set_xlabel("Periodo",fontsize=14)
+    ax.title.set_text(titulo)
+    plt.xticks(rotation='vertical')
+    ax.set_ylabel("Llamadas perdidas/mes",color="#1f77b4",fontsize=14)
+
+    ax2=ax.twinx()
+    ax2.plot(tabla[['Duración llamada prom (min)']], color="r", marker="x")
+    ax2.set_ylim([1,2.5])
+    ax2.set_ylabel("Duración llamada prom (min)",color="r",fontsize=14)
+
+# graficos de cantidad de llamadas y uso de CR
+def DLG_cantidad(dataset, inicio=None, fin=None, ax= None, titulo=None, **kwargs):
+    # Selecciono un rango de fechas para la función
+    db1 = corte_dataset(dataset, inicio, fin)
+    
+    # Métricas #######
+    metricas = tablaCDR_general(db1) 
+    dum_t1 = metricas['Primera observacion']
+    dum_t2 = metricas['Ultima observacion']
+    dum_dt = metricas['Duracion en dias']
+    
     # Gráfico de llamadas por hora ########################################################
     ax = ax or plt.gca()  # si hay ax se queda, si no lo hay lo creo
-    (db1.groupby(db1.Fecha.dt.hour).count().Fecha/dum_dt.days).plot(
+    (db1.groupby(db1.Fecha.dt.hour).count().Fecha/dum_dt).plot(
         ax = ax,
         label=' {} al {}. '.format(dum_t1.strftime('%d-%b-%Y'), dum_t2.strftime('%d-%b-%Y')),
         **kwargs)
     plt.xlabel('Hora del día')
     plt.ylabel('Cantidad de llamadas')
-    plt.title('Cantidad de llamadas según hora')
+    plt.title(titulo)
     plt.ylim([0,90])
     plt.grid()
     plt.legend(loc='lower right')
     
-def DLG_duracion(inicio, fin, ax= None, **kwargs):
-    # inicio y fin son fechas 
-    db1 = BD.loc[
-        (BD.Fecha > pd.to_datetime(inicio)) &
-        (BD.Fecha < pd.to_datetime(fin))]
-    # Periodo de estudio ########################################################
-    dum_t1 = db1.Fecha.describe()['first']
-    dum_t2 = db1.Fecha.describe()['last']
-
-    # Cantidad de días del estudio ########################################################
-    dum_dt = db1.Fecha.describe()['last'] - db1.Fecha.describe()['first']
+def DLG_duracion(dataset, inicio=None, fin=None, ax= None, titulo=None, **kwargs):
+    # Selecciono un rango de fechas para la función
+    db1 = corte_dataset(dataset, inicio, fin)
+    
+    # Métricas #######
+    metricas = tablaCDR_general(db1) 
+    dum_t1 = metricas['Primera observacion']
+    dum_t2 = metricas['Ultima observacion']
+    dum_dt = metricas['Duracion en dias']
+    
     # Gráfico de llamadas por hora ########################################################
     ax = ax or plt.gca()  # si hay ax se queda, si no lo hay lo creo
-    ((db1.groupby(db1.Fecha.dt.hour)).Duracion.sum()/(dum_dt.days*60)).plot(
+    ((db1.groupby(db1.Fecha.dt.hour)).Duracion.sum()/(dum_dt*60)).plot(
         ax = ax,
         label=' {} al {}. '.format(dum_t1.strftime('%d-%b-%Y'), dum_t2.strftime('%d-%b-%Y')),
         **kwargs)
     plt.xlabel('Hora del día')
     plt.ylabel('Duración acumulada de llamadas en minutos')
-    plt.title('Duración de llamadas por hora')
+    plt.title(titulo)
     plt.ylim([0,140])
     plt.grid()
-    plt.legend(loc='upper left')
-
+    plt.legend(loc='lower right')
 
 # Defino algunos bucles
-
-def bucle_DLG_cantidad(fecha, repeticiones=4):
-    Start1 = pd.to_datetime(fecha)
-    n=0
+def bucle_DLG_cantidad(dataset, inicio=None, fin=None, repeticiones=4):
+    inicio = pd.to_datetime(inicio)
     for x in range(repeticiones):
-        inicio  = Start1 + timedelta(weeks=n)
-        fin     = inicio + timedelta(weeks=1)
-        DLG_cantidad(inicio, fin)
-        n=n+1
+        print('Vuelta:', x)
+        t1     = inicio + timedelta(days=x*7)
+        t2     = t1 + timedelta(days=7)
+        DLG_cantidad(dataset, t1, t2)
 
-def bucle_DLG_duracion(fecha, repeticiones=4):
-    Start1 = pd.to_datetime(fecha)
-    n=0
+def bucle_DLG_duracion(dataset, inicio=None, fin=None, repeticiones=4):
+    inicio = pd.to_datetime(inicio)
     for x in range(repeticiones):
-        inicio  = Start1 + timedelta(weeks=n)
-        fin     = inicio + timedelta(weeks=1)
-        DLG_duracion(inicio, fin)
-        n=n+1
+        print('Vuelta:', x)
+        t1     = inicio + timedelta(days=x*7)
+        t2     = t1 + timedelta(days=7)
+        DLG_duracion(dataset, t1, t2)
+
+
--- Analisis llamadas 131/Scripts/Resultados.py
+++ Analisis llamadas 131/Scripts/Resultados.py
@@ -7,41 +7,118 @@ exec(open('Funciones_descriptoras.py').read())
 
 
 print('Marzo 2019 vs 2020')
-bucle_DLG_cantidad('2019-03-04')
+bucle_DLG_cantidad(BD, '2019-03-04')
 plt.savefig('../Resultado_script/Llamadas_Cantidad_Marzo2019.png')
 plt.clf()
 #plt.show()
-bucle_DLG_cantidad('2020-03-02')
+bucle_DLG_cantidad(BD, '2020-03-02')
 plt.savefig('../Resultado_script/Llamadas_Cantidad_Marzo2020.png')
 plt.clf()
 #plt.show()
 
 print('Abril 2019 vs 2020')
-bucle_DLG_cantidad('2019-04-01')
+bucle_DLG_cantidad(BD, '2019-04-01')
 plt.savefig('../Resultado_script/Llamadas_Cantidad_Abril2019.png')
 plt.clf()
 #plt.show()
-bucle_DLG_cantidad('2020-04-06')
+bucle_DLG_cantidad(BD, '2020-04-06')
 plt.savefig('../Resultado_script/Llamadas_Cantidad_Abril2020.png')
 plt.clf()
 #plt.show()
 
-
-bucle_DLG_duracion('2019-03-04')
+bucle_DLG_duracion(BD, '2019-03-04')
 plt.savefig('../Resultado_script/Llamadas_duracion_Marzo2019.png')
 plt.clf()
 #plt.show()
-bucle_DLG_duracion('2020-03-02')
+bucle_DLG_duracion(BD, '2020-03-02')
 plt.savefig('../Resultado_script/Llamadas_duracion_Marzo2020.png')
 plt.clf()
 #plt.show()
 
-bucle_DLG_duracion('2019-04-01')
+bucle_DLG_duracion(BD, '2019-04-01')
 plt.savefig('../Resultado_script/Llamadas_duracion_Abril2019.png')
 plt.clf()
 #plt.show()
-bucle_DLG_duracion('2020-04-06')
+bucle_DLG_duracion(BD, '2020-04-06')
 plt.savefig('../Resultado_script/Llamadas_duracion_Abril2020.png')
 plt.clf()
 #plt.show()
+
+DLG_duracion(BD, '2018-03-20', '201805-15')
+DLG_duracion(BD, '2019-03-20', '201905-15')
+DLG_duracion(BD, '2020-03-20', '2020-05-15', titulo = 'Duración de llamadas por hora \n Mismo periodo 2018, 2019 y 2020')
+plt.savefig('../Resultado_script/Duración por hora.png')
+# plt.show()
+plt.clf()
+
+DLG_cantidad(BD, '2018-03-20', '201805-15')
+DLG_cantidad(BD, '2019-03-20', '201905-15')
+DLG_cantidad(BD, '2020-03-20', '2020-05-15', titulo = 'Cantidad de llamadas por hora \n Mismo periodo 2018, 2019 y 2020')
+plt.savefig('../Resultado_script/Cantidad por hora.png')
+# plt.show()
+plt.clf()
+
+# print(tablaCDR_general(BD, '2019-03-20', '201905-15'))
+# print(tablaCDR_general(BD, '2020-03-20', '202005-15'))
+
+
+
+# Gráficos año 2018
+tab2018 = tabla_indicadores(BD ,start='2018-01-02', end = '2019-01-01', freq='30D')
+tab = tab2018
+grafo_usoyllamadas(tab, 'Uso CR y cantidad llamadas \n 2018')
+# plt.show()
+plt.savefig('../Resultado_script/Usoyllamadas_2018.png',bbox_inches="tight")
+plt.clf()
+grafo_perdidasyduracion(tab, 'Llamadas perdidas y duración \n 2018')
+# plt.show()
+plt.savefig('../Resultado_script/Perdidasyduracion_2018.png',bbox_inches="tight")
+plt.clf()
+
+# Gráficos año 2019
+tab2019 = tabla_indicadores(BD ,start='2019-01-03', end = '2020-01-01', freq='30D')
+tab = tab2019
+grafo_usoyllamadas(tab, 'Uso CR y cantidad llamadas \n 2019')
+# plt.show()
+plt.savefig('../Resultado_script/Usoyllamadas_2019.png',bbox_inches="tight")
+plt.clf()
+grafo_perdidasyduracion(tab, 'Llamadas perdidas y duración \n 2019')
+# plt.show()
+plt.savefig('../Resultado_script/Perdidasyduracion_2019.png',bbox_inches="tight")
+plt.clf()
+
+# Gráficos año 2020
+tab2020 = tabla_indicadores(BD ,start='2020-01-02', end = '2020-05-20', freq='10D')
+tab = tab2020
+grafo_usoyllamadas(tab, 'Uso CR y cantidad llamadas \n 2020')
+# plt.show()
+plt.savefig('../Resultado_script/Usoyllamadas_2020.png',bbox_inches="tight")
+plt.clf()
+grafo_perdidasyduracion(tab, 'Llamadas perdidas y duración \n 2020')
+# plt.show()
+plt.savefig('../Resultado_script/Perdidasyduracion_2020.png',bbox_inches="tight")
+plt.clf()
+
+
+tab = tabla_indicadores(BD ,start='2019-10-02', end = '2019-12-10', freq='5D')
+tab = tab
+grafo_usoyllamadas(tab, 'Uso CR y cantidad llamadas \n Marchas 2019')
+plt.savefig('../Resultado_script/Usoyllamadas_Marchas2019.png',bbox_inches="tight")
+plt.clf()
+grafo_perdidasyduracion(tab, 'Llamadas perdidas y duración \n Marchas 2020')
+# plt.show()
+plt.savefig('../Resultado_script/Perdidasyduracion_Marchas2019.png',bbox_inches="tight")
+plt.clf()
+
+
+# tab = pd.DataFrame({'2018'       : tablaCDR_general(BD, '2018.03.01', '2018-11-30'),
+#               'Mar2019'    : tablaCDR_general(BD, '2019.03.01', '2019-03-30'),
+#               'Abr2019'    : tablaCDR_general(BD, '2019.04.01', '2019-04-30'),
+#               'May2019'    : tablaCDR_general(BD, '2019.05.01', '2019-05-30'),
+#               'Nov2019'    : tablaCDR_general(BD, '2019.09.01', '2019-09-30'),
+#               'Mar2020'    : tablaCDR_general(BD, '2020.03.01', '2020-03-15'),
+#               'Abr2020'    : tablaCDR_general(BD, '2020.04.01', '2020-04-30'),
+#               'May2020'    : tablaCDR_general(BD, '2020.05.01', '2020-05-30')
+#              }
+#             ).T
 
